434ce0159bf3175894e660f05369249bce726eca
hatem156 <126658469+hatem156@users.noreply.github.com>
2023-04-10 08:05:17 +0200

    finish sign in page

---
+++ lib/pages/sign_in.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+
+import 'package:insta_app/pages/register.dart';
+import 'package:insta_app/responsive/mobile.dart';
+import 'package:insta_app/responsive/responsive.dart';
+import 'package:insta_app/responsive/web.dart';
+import 'package:insta_app/widgets/colors.dart';
+
+import '../widgets/contants.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool isVisable = false;
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool isLoading = false;
+
+  
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+        final double widthscreen = MediaQuery.of(context).size.width;
+    //final googleSignInProvider = Provider.of<GoogleSignInProvider>(context);
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: mobileBackgroundColor,
+          title: const Text("Sign in"),
+        ),
+        backgroundColor: mobileBackgroundColor,
+        body: Center(
+            child: Padding(
+          padding: widthscreen > 600? EdgeInsets.symmetric(horizontal: widthscreen*0.20 ): const EdgeInsets.all(33.0),
+          child: SingleChildScrollView(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const SizedBox(
+                height: 64,
+              ),
+              TextField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  obscureText: false,
+                  decoration: decorationTextfield.copyWith(
+                      hintText: "Enter Your Email : ",
+                      suffixIcon: const Icon(Icons.email))),
+              const SizedBox(
+                height: 33,
+              ),
+              TextField(
+                  controller: passwordController,
+                  keyboardType: TextInputType.text,
+                  obscureText: isVisable ? false : true,
+                  decoration: decorationTextfield.copyWith(
+                      hintText: "Enter Your Password : ",
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              isVisable = !isVisable;
+                            });
+                          },
+                          icon: isVisable
+                              ? const Icon(Icons.visibility)
+                              : const Icon(Icons.visibility_off)))),
+              const SizedBox(
+                height: 33,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  // await signIn();
+                  // if (!mounted) return;
+                  // showSnackBar(context, "Done ... ");
+                  Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(builder: (context) => const Responsive(
+                                mymobile: MobileScreen(),
+                                myweb: WebScreen(),
+                              )),
+                            );
+                },
+                style: ButtonStyle(
+                 // backgroundColor: MaterialStateProperty.all(bTNgreen),
+                  padding: MaterialStateProperty.all(const EdgeInsets.all(12)),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8))),
+                ),
+                child: isLoading
+                    ? const CircularProgressIndicator(
+                        color: Colors.white,
+                      )
+                    : const Text(
+                        "Sign in",
+                        style: TextStyle(fontSize: 19),
+                      ),
+              ),
+              const SizedBox(
+                height: 9,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Do not have an account?",
+                      style: TextStyle(fontSize: 18)),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Register()),
+                        );
+                      },
+                      child: const Text('sign up',
+                          style: TextStyle(
+                              fontSize: 18,
+                              decoration: TextDecoration.underline))),
+                ],
+              ),
+            ]),
+          ),
+        )));
+  }
+}
