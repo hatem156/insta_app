2263d4fbb5ced95d5ec5cb89450b051530312c15
hatem156 <126658469+hatem156@users.noreply.github.com>
2023-04-10 08:00:21 +0200

    finish add post page

--- lib/pages/add_pic.dart
+++ lib/pages/add_pic.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/colors.dart';
+
 class Addpost extends StatefulWidget {
   const Addpost({super.key});
 
@@ -10,9 +12,14 @@ class Addpost extends StatefulWidget {
 class _AddpostState extends State<Addpost> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Add post"),
+    return  Scaffold(
+      backgroundColor: mobileBackgroundColor,
+      body: Center(
+        child: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.upload,
+          size: 55,)
+          ),
       ),
     );
   }
